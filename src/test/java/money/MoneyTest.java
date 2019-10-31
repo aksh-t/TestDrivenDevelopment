f7894c15b83ff02ed25c20c44e60c07dfc2bd992
aksh-t <tanno.ao@gmail.com>
2019-11-01 03:22:15 +0900

    change Type Dollar => Money in test

--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 public class MoneyTest {
     @Test
     public void testMultipication() {
-        Dollar five = Money.dollar(5);
+        Money five = Money.dollar(5);
         assertEquals(new Dollar(10), five.times(2));
         assertEquals(new Dollar(15), five.times(3));
     }
